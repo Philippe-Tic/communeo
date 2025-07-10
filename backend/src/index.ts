deb5453ebd59b1477c508bcc3c26a089e8a99467
Philippe Chevreul <philippe.chevreul@cleyrop.com>
2025-07-10 14:10:11 +0200

    feat: test2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,6 @@ export default {
    *
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
-   * test
    */
   bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
 };
